be94d06afdc398f2cdd0677ef0655b727fa77593
Overblade <57635666+Overblade@users.noreply.github.com>
2020-05-02 00:00:26 +0200

    Overblade test (#1)
    * Test
    
    * + Font documentation finished
    
    + Global.h
    - nitro_if.h corrected
    
    * Rename Font.h to src/Font.h
    
    * Neccessary files
    
    * Rename Global.h to src/Global.h
    
    * Rename Memory.h to src/Memory.h
    
    * Rename nitro_if.h to src/nitro_if.h
    
    * Font documentation finished
    
    * Fixes tab spaces

---
+++ src/nitro_if.h
@@ -0,0 +1,32 @@
+#ifndef NITRO_IF_H
+#define NITRO_IF_H
+
+typedef void* NITRO_TYPE_REPLACEMENT;
+
+#ifdef NITRO_NO_SDK
+
+	#warning("Project built without Nitro-SDK/NNS, replacing interface")
+
+	typedef NITRO_TYPE_REPLACEMENT NNSFndHeapHandle;
+	typedef NITRO_TYPE_REPLACEMENT OSMutex;
+	typedef NITRO_TYPE_REPLACEMENT OSArenaID;
+	typedef NITRO_TYPE_REPLACEMENT NNSFndList;
+	typedef NITRO_TYPE_REPLACEMENT NNSG2dFont;
+	typedef NITRO_TYPE_REPLACEMENT NNSG2dGlyph;
+
+	typedef unsigned char u8;
+	typedef unsigned short u16;
+	typedef unsigned int u32;
+	typedef unsigned long long int u64;
+
+	typedef signed char s8;
+	typedef signed short s16;
+	typedef signed int s32;
+	typedef signed long long int s64;
+
+#else
+	#include "nitro.h"
+	#include "nnsys.h"
+#endif
+
+#endif	// NITRO_IF_H
