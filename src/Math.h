fa8c0d100abaf2af3bbfc3fe8a021dc3028ae391
Overblade <57635666+Overblade@users.noreply.github.com>
2020-05-14 13:51:49 +0200

    Fixed formatting

--- src/Math.h
+++ src/Math.h
@@ -3,13 +3,17 @@
 
 #include "nitro_if.h"
 
-namespace Math
-{
+/*
+	General math functions
+*/
+namespace Math {
+	
 	s32 sqrt64(u32 lo, u32 hi);
 
 	inline s32 sqrt64(s64 val) {
-		return sqrt64((u32)val, (u32)val >> 32);
+		return sqrt64(val & 0xFFFFFFFF, val >> 32);
 	}
+	
 }
 
 #endif	// NSMB_MATH_INCLUDED
