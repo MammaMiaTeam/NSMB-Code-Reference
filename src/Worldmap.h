3a3cdd25f6609b9d6dc601f7a9f556cbd4d6511b
Cortex-A32 <57635666+Overblade@users.noreply.github.com>
2020-09-17 01:14:16 +0200

    WorldmapPlayerModel done

--- src/Worldmap.h
+++ src/Worldmap.h
@@ -236,7 +236,7 @@ public:
 	u32 wmPathFiles[9];
 
 	//021a8154
-	u32 worldmapModelFiles[8][16];
+	u32 worldmapModelFileIDs[8][16];
 
 	//021add50
 	u8 worldStarCoinsCompleted;//Bitmask (1 << world)
@@ -497,13 +497,105 @@ public:
 };
 
 
+
+//vtable 021a9530
+class WorldmapPlayerModel
+{
+public:
+
+	struct ModelConfig {
+		s8 sizeIndex;
+		s8 texPalette;
+	};
+
+	struct WMAnimation {
+		u32 animFileID;
+		u32 animIndex;			//NSBCA animation index
+		fx32 speed;
+		FrameCtrl::Type type;
+		u32 unknown;
+		u32 headAnimID;
+	};
+
+	enum class WMPowerupState : s8 {
+		Small = 0,
+		Super,
+		Fire,
+		Unused0,	//Identical to Small
+		Shell,
+		Unused1		//Identical to Super
+	};
+
+	PlayerModel model;				//Player model
+	ModelConfig* config;			//Configuration for the model depending on powerup state
+	u32 animID;						//Current main animation ID
+	u32 headAnimID;					//Current head animation ID
+	u8 playerID;					//Player ID
+	s8 sizeState;					//Size: 0=standard,1=small
+	WMPowerupState powerupState;	//Current powerup state
+
+	
+	//021a39d0
+	static WMAnimation animations[6];
+
+	//021a39bc
+	static u32 idleAnimationIDs[2];		//Not applicable to size state 1
+
+	//021a39b4
+	static u32 walkAnimationIDs[2];
+
+	//021a39ac
+	static u32 runAnimationIDs[2];
+
+	//021a39c4
+	static ModelConfig modelConfigs[6];
+
+
+	//0219d710
+	WorldmapPlayerModel();
+
+	//D0:0219d6c0
+	//D1:0219d6ec
+	virtual ~WorldmapPlayerModel();
+
+	//0219d5e8
+	bool create(u8 playerID, s8 playerPowerup, u32 animID);
+
+	//0219d574
+	void render(MtxFx43* modelMatrix, Vec3* scale);
+
+	//0219d550
+	void update(bool skip);
+
+	//0219d440
+	void setAnimation(u32 animID, bool continueAnimation);
+
+	//0219d3b8
+	void setHeadAnimation(u32 headAnimID);
+
+	//0219cbdc
+	static bool loadFiles();
+
+};
+
+
+
 //vtable: 021a9040
 class WorldmapActor : public Actor
 {
 public:
 
-	//02199d58
+	//C1:02199d58
+	//C3:0219ab98
 	WorldmapActor();
+
+	//D0:02199788
+	//D1:02199a74
+	virtual ~WorldmapActor() override;
+
+	//021983cc
+	virtual s32 onCreate() override;
+
 };
 
 
