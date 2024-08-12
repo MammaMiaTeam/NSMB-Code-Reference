fad540748f1ff122e51cda390d39b22649c20b17
Chris Rock <76601747+pete420griff@users.noreply.github.com>
2024-08-12 18:52:59 -0400

    Added PipeEnemySpawner

---
+++ include/nsmb/stage/actors/ov22/pipeenemyspawner.hpp
@@ -0,0 +1,76 @@
+#pragma once
+#include "nsmb.hpp"
+
+// vtable at 0213C554 (ov22)
+class PipeEnemySpawner : public StageEntity
+{
+public:
+
+	using StateFunction = bool(PipeEnemySpawner::*)();
+
+	enum class EnemyTypes : u32 {
+		Goomba,
+		BobOmb
+	};
+
+	// D0: 0213Bf5C
+	// D1: 0213BF00
+	virtual ~PipeEnemySpawner();
+
+	// 0213BFD8
+	s32 onCreate() override;
+
+	// 0213BFC0
+	s32 onUpdate() override;
+
+	// 0213C3D0
+	bool switchState(const StateFunction& function);
+	// 0213C388
+	void updateState();
+
+	// 0213C034
+	bool mainState();
+
+	// 0213C2EC
+	void spawnGoomba();
+	// 0213C248
+	void spawnBobOmb();
+
+	// 0213C508
+	static void init(); // ?
+
+	static constexpr u16 objectID = 64;
+
+	static constexpr u16 updatePriority = objectID;
+	static constexpr u16 renderPriority = 78;
+
+	// 0213C548
+	static const ActorProfile profile;
+
+	// 0213C680
+	static const StateFunction sMain;
+
+	// 0213C4C0
+	static const u16 goombaCooldowns[4]; // time between each goomba spawn ...3 and 4 unused?? or unused bob-omb cooldown maybe?
+	// 0213C4C4
+	//static const u16 unusedbobOmbCooldowns[2]; 
+	// 0213C4C8
+	static const fx32 goombaOffsetsX[4];
+	// 0213E4E8
+	static const fx32 goombaOffsetsY[4];
+	// 0213C4F8
+	static const fx32 bobOmbOffsetsX[4];
+	// 0213C4D8
+	static const fx32 bobOmbOffsetsY[4];
+
+	//NTR_INLINE static const u32 bobOmbCooldown = 0x153;
+
+	const StateFunction* updateFunction;
+
+	EnemyTypes enemyType;
+	u16 spawnCooldown;
+	u16 spawnCounter;  
+	s8 updateStep;
+
+};
+NTR_SIZE_GUARD(PipeEnemySpawner, 0x404);
