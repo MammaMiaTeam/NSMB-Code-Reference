76723e39d784a026da703b6ccdedbc6fc6c7139b
TheGameratorT <tiagolsilva14@gmail.com>
2024-10-22 00:53:26 +0100

    Add Rectangle class

--- include/nsmb/math.hpp
+++ include/nsmb/math.hpp
@@ -4,5 +4,6 @@
 #include "math/math.hpp"
 #include "math/matrix.hpp"
 #include "math/random.hpp"
+#include "math/rectangle.hpp"
 #include "math/springfunction.hpp"
 #include "math/vector.hpp"
---
+++ include/nsmb/math/rectangle.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+template<class T>
+class Rectangle {
+public:
+	T x;
+	T y;
+	T width;
+	T height;
+};
