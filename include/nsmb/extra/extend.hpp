e534f465bc8e320aa0732f38f6ac56cc1500eb8e
pete420griff <willsmithofficial2222@gmail.com>
2025-11-09 17:44:57 -0500

    Make extend directory configurable

--- include/nsmb/extra/extend.hpp
+++ include/nsmb/extra/extend.hpp
@@ -2,9 +2,11 @@
 
 #include "preprocessor.hpp"
 
+#ifndef NTR_EXT_DIR
+	#define NTR_EXT_DIR nsmb/extend
+#endif
 
-
-#define __NTR_MAKE_EXT_PATH(t) PP_STR(nsmb/extend/t.inl)
+#define __NTR_MAKE_EXT_PATH(t) PP_STR(NTR_EXT_DIR/t.inl)
 
 #define NTR_EXT_EXISTS(t)	PP_HAS_INCLUDE(__NTR_MAKE_EXT_PATH(t))
 #define NTR_EXT_INCLUDE(t)	__NTR_MAKE_EXT_PATH(t)
